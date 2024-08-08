1749f98a561973b2514813430b0ef20c82c6b4e5
gheageltraa <ghea.geltra@gmail.com>
2024-08-08 14:10:06 +0700

    Contact form

---
+++ csc-website/src/app/components/Fragment/ContactForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
